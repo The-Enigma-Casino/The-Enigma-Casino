1122c54845f64c9b0a75e0013cfe005af2eb51fd
Raquel <rlopber070@g.educaand.es>
2025-02-13 19:56:56 +0100

    Prueba

--- backend/the-enigma-casino-server/Models/Database/MyDbContext.cs
+++ backend/the-enigma-casino-server/Models/Database/MyDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace the_enigma_casino_server.Models.Database;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class MyDbContext
-{
+namespace the_enigma_casino_server.Models.Database;
 
+public class MyDbContext : DbContext
+{\
+    private const string DATABASE_PATH = "enigmacasino.db";
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        //AppDomain obtiene el directorio donde se ejecuta la aplicación
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        // Se configura Sqlite como proveedor de BD pasando la ruta de archivo ("vhypergames.db) en el directorio base de la aplicacion
+#if DEBUG
+        options.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+#elif RELEASE
+        options.UseMySql(Environment.GetEnvironmentVariable("DB_CONFIG"), ServerVersion.AutoDetect(Environment.GetEnvironmentVariable("DB_CONFIG")));
+#endif
+    }
 }

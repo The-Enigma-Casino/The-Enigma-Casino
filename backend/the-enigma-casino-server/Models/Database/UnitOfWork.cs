816a0370fd6165b9a9b0da205e35ab0168012727
Raquel <rlopber070@g.educaand.es>
2025-02-14 20:25:13 +0100

    Unit of Work

---
+++ backend/the-enigma-casino-server/Models/Database/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using the_enigma_casino_server.Models.Database.Repositories;
+
+namespace the_enigma_casino_server.Models.Database;
+
+public class UnitOfWork
+{
+    private readonly MyDbContext _context;
+
+    private UserRepository _userRepository;
+
+    public UserRepository UserRepository => _userRepository ??= new UserRepository(_context);
+
+    public UnitOfWork(MyDbContext myDbContext)
+    {
+        _context = myDbContext;
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+}

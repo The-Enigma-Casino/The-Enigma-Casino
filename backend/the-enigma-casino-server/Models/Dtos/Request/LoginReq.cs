db971b2cb90b89d9c6e3bc2358016feabe3cfadd
Raquel <rlopber070@g.educaand.es>
2025-02-14 20:25:06 +0100

    Requests

---
+++ backend/the-enigma-casino-server/Models/Dtos/Request/LoginReq.cs
@@ -0,0 +1,6 @@
+﻿namespace the_enigma_casino_server.Models.Dtos.Request
+{
+    public class LoginReq
+    {
+    }
+}
---
+++ backend/the-enigma-casino-server/Models/Dtos/Request/RegisterReq.cs
@@ -0,0 +1,12 @@
+﻿namespace the_enigma_casino_server.Models.Dtos.Request;
+
+public class RegisterReq
+{
+    public string NickName { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Dni { get; set; }
+    public string Country { get; set; }
+    public string Address { get; set; }
+}

ca88c783992cf40b426dbc3eb63c3a560b26946f
Raquel <rlopber070@g.educaand.es>
2025-02-14 20:24:57 +0100

    Controladores

---
+++ backend/the-enigma-casino-server/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using the_enigma_casino_server.Models.Database;
+using the_enigma_casino_server.Models.Dtos.Request;
+using the_enigma_casino_server.Services;
+using the_enigma_casino_server.Utilities;
+
+namespace the_enigma_casino_server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : BaseController
+{
+    public UserService _userService;
+    public AuthController(UserService userService)
+    {
+
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<string>> Register([FromBody] RegisterReq request)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(request.NickName) || string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Dni)) 
+                return BadRequest("Alguno de los campos enviados está vacío.");
+
+
+            // Ver si existe el usuario
+            (bool exists, string message) = await _userService.ExistsUser(request.NickName, request.Email, request.Dni);
+
+            if (exists)
+                return BadRequest(message);
+
+
+            // Crear usuario
+            return Ok("Usuario registrado exitosamente.");
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+}
---
+++ backend/the-enigma-casino-server/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace the_enigma_casino_server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    protected int GetUserId()
+    {
+        var userIdClaim = User.FindFirst("id");
+        if (userIdClaim == null)
+        {
+            throw new UnauthorizedAccessException("Usuario no autenticado");
+        }
+        return int.Parse(userIdClaim.Value);
+    }
+}
+

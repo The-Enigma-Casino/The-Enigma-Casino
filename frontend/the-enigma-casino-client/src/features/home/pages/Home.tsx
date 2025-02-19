4a69b2c835506fbbf8d3a3b228327d3914d52b77
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-02-19 20:06:53 +0100

    RootLayout

--- frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
+++ frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
@@ -1,10 +1,8 @@
-import Footer from "../../../components/layouts/footer/Footer";
 
 function Home() {
   return (
     <>
       <h1>Hola mundo</h1>
-      <Footer />
     </>
   );
 }
--- frontend/the-enigma-casino-client/src/layouts/RootLayout.tsx
+++ frontend/the-enigma-casino-client/src/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/layouts/footer/Footer";
 
 function RootLayout() {
   return (
     <main>
       <Outlet />
+      <Footer />
     </main>
   );
 }

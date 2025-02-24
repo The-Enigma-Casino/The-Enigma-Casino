b79a413961d2b0fa65972b9d0468111e698b8b41
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-02-24 19:27:45 +0100

    Axios y Tailwind

---
+++ frontend/the-enigma-casino-client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Principal: '#74c410',
+        Coins: '#ffdf31',
+        'Background-Chat': '#d4edb5',
+        'Background-Overlay': '#2e2e2e',
+        'Background-Page': '#0f0f0f',
+        'Color-Edit': '#2457c5',
+        'Color-Cancel': '#ce2e2e',
+        'Green-lines': '#7ff803',
+        'Black-color': '#000814',
+        'Grey-color': '#505050',
+      },
+    },
+  },
+  plugins: [],
+}

1c0e267acb6cd2a2b76b390240be9bdb662b0559
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-02-25 19:17:27 +0100

    Confirmacion de email

--- frontend/the-enigma-casino-client/src/features/auth/components/layouts/ConfirmationComponent.tsx
+++ frontend/the-enigma-casino-client/src/features/auth/components/layouts/ConfirmationComponent.tsx
@@ -1,16 +1,33 @@
+import { useEffect } from "react";
 import classes from "./ConfirmationComponent.module.css";
+import { confirmEmailFx } from "../../actions/authActions";
+import { useNavigate } from "react-router-dom";
 
 interface ConfirmationProps {
   token: string;
 }
 
-function ConfirmationComponent({ token }: ConfirmationProps) {
+const ConfirmationComponent = ({ token }: ConfirmationProps) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      confirmEmailFx(token);
+
+      const timer = setTimeout(() => {
+        navigate("/auth/login");
+      }, 300);
+
+      return () => clearTimeout(timer);
+    }
+  }, [token, navigate]);
+
   return (
     <div className={classes.container}>
       <h1 className={classes.title}>Confirmación de Email</h1>
-      <img src="/img/jumping-elf.webp" alt="Mascota" className={classes.elf}/>
+      <img src="/img/jumping-elf.webp" alt="Mascota" className={classes.elf} />
     </div>
   );
-}
+};
 
 export default ConfirmationComponent;

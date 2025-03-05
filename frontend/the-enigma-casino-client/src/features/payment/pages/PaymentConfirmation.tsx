daef94f9f64fca14f0a1cd32343ac0b3ee9595ff
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-05 20:08:21 +0100

    Comentado por ahora

--- frontend/the-enigma-casino-client/src/features/payment/pages/PaymentConfirmation.tsx
+++ frontend/the-enigma-casino-client/src/features/payment/pages/PaymentConfirmation.tsx
@@ -1,4 +1,4 @@
-import CoinsCard from "../../catalog/components/CoinsCard";
+// import CoinsCard from "../../catalog/components/CoinsCard";
 
 function PaymentConfirmation() {
   return (
@@ -8,14 +8,14 @@ function PaymentConfirmation() {
           PAGO REALIZADO
         </h1>
         <div>
-          <CoinsCard
+          {/* <CoinsCard
             key={1}
             id={1}
             price={1000}
             quantity={100}
             image={"/img/pack1.webp"}
             offer={0}
-          />
+          /> */}
           {/* Mi nuevo componente */}
         </div>
         <h1 className="text-Principal text-[6rem] font-bold text-center font-reddit">

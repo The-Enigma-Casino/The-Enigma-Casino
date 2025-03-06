3e1dcebebf22adebaa29a3ee74b901dffdc8f7e8
Raquel <rlopber070@g.educaand.es>
2025-03-06 19:47:25 +0100

    Endpoints de coinspack y payment

--- frontend/the-enigma-casino-client/src/config.ts
+++ frontend/the-enigma-casino-client/src/config.ts
@@ -6,4 +6,11 @@ export const API_BASE_URL: string = `${BASE_URL}api/`;
 export const LOGIN_ENDPOINT = `${API_BASE_URL}Auth/login`;
 export const REGISTER_ENDPOINT = `${API_BASE_URL}Auth/register`;
 export const CONFIRM_EMAIL_ENDPOINT = `${API_BASE_URL}Auth/confirm-email`;
+
+/* ENDPOINTS DE COINSPACK */
 export const COINS_PACK = `${API_BASE_URL}Catalog/coins-packs`;
+export const COINS_PACK_ID = `${API_BASE_URL}Catalog/coins-packs-by-id`;
+
+/* ENDPOINTS DE PAYMENT */
+export const PAYMENT_STATUS = `${API_BASE_URL}Stripe/status/`;
+export const EMBBEDED_CHECKOUT = `${API_BASE_URL}Stripe/embedded-checkout/`;

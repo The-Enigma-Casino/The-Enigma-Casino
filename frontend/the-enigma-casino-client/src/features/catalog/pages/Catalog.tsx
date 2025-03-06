8a3eee13d437484f7c92cbed629c283934b8a994
José Molina <josemolinamelendez@gmail.com>
2025-03-06 20:25:40 +0100

    Features/catalog
    Add fetch with endpoint

--- frontend/the-enigma-casino-client/src/features/catalog/pages/Catalog.tsx
+++ frontend/the-enigma-casino-client/src/features/catalog/pages/Catalog.tsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CoinsCard from "../components/CoinsCard";
 import PaymentMethod from "../components/PaymentMethod";
 import { CoinsPack } from "../models/CoinsPack.interface";
+import { useUnit } from "effector-react";
+import { getCoinsPacksFx } from "../actions/catalogActions";
+import { $coinsPacks } from "../store/paymentStore"
+
 
 const Catalog = () => {
   const [selectedCard, setSelectedCard] = useState<number | null>(null);
+  const coinsPacks = useUnit($coinsPacks);
 
+  useEffect(() => {
+    getCoinsPacksFx();
+  }, []);
 
-  const packs: CoinsPack[] = [
-    { id: 1, price: 1000, quantity: 100, image: "/img/pack1.webp", offer: 100 },
-    { id: 2, price: 2000, quantity: 200, image: "/img/pack1.webp", offer: 0 },
-    { id: 3, price: 5000, quantity: 500, image: "/img/pack1.webp", offer: 0 },
-    { id: 4, price: 10000, quantity: 1000, image: "/img/pack1.webp", offer: 0 },
-    { id: 5, price: 20000, quantity: 2000, image: "/img/pack1.webp", offer: 0 },
-    { id: 6, price: 50000, quantity: 5000, image: "/img/pack1.webp", offer: 0 },
-  ];
 
   const handleSelect = (id: number) => {
     setSelectedCard(id === selectedCard ? null : id);
@@ -23,7 +23,7 @@ const Catalog = () => {
   return (
     <div className="flex flex-col md:flex-row bg-Background-Page h-full">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4 md:gap-6 md:p-6 flex-grow">
-        {packs.map((pack) => (
+        {coinsPacks.map((pack) => (
           <CoinsCard
             key={pack.id}
             id={pack.id}

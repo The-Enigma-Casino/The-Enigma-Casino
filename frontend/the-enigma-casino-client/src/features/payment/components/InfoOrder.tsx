ad1eed16a9c8c9ad48fb365a35cf2fcabba3001f
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-06 18:30:33 +0100

    InfoOrder casi listo

--- frontend/the-enigma-casino-client/src/features/payment/components/InfoOrder.tsx
+++ frontend/the-enigma-casino-client/src/features/payment/components/InfoOrder.tsx
@@ -1,27 +1,37 @@
 import CoinsCard from "../../catalog/components/CoinsCard";
+import { OrderDto } from "../models/OrderDto.interface";
 
-function InfoOrder() {
+interface InfoOrderProps {
+  order: OrderDto;
+}
 
-
-
+const InfoOrder: React.FC<InfoOrderProps> = ({ order }) => {
+  const date = `Fecha: ${order.paidDate.toLocaleDateString()}`;
+  const paidWith = `Pagado con: ${order.payMode}`;
+  const totalPaid = `Total pagado: ${order.coinsPack.price}`;
 
   return (
     <div>
       <CoinsCard
-        id={1}
-        price={1000}
-        quantity={100}
-        image={"/img/pack1.webp"}
-        offer={20}
-        // size={"large"}
+        id={order.id}
+        price={order.coinsPack.price}
+        quantity={order.coinsPack.quantity}
+        image={order.coinsPack.image}
+        offer={order.coinsPack.offer}
         isSelected={false}
         onSelect={() => {}}
       />
       <div>
-
+        <div>
+          <p>+{order.coinsPack.price}</p>
+          <img src="/svg/coins.svg" alt="coins" />
+        </div>
+        <p>{date}</p>
+        <p>{paidWith}</p>
+        <p>{totalPaid}</p>
       </div>
     </div>
   );
-}
+};
 
 export default InfoOrder;

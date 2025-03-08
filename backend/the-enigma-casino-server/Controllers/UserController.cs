424b6cb49288ef92496013829b85351c6730cea3
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-08 19:33:54 +0100

    GetCoins

---
+++ backend/the-enigma-casino-server/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using the_enigma_casino_server.Services;
+
+namespace the_enigma_casino_server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : BaseController
+{
+    public UserService _userService;
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("coins")]
+    public async Task<ActionResult<int>> GetCoins()
+    {
+        try
+        {
+            int id = GetUserId(); 
+            var coins = await _userService.GetCoins(id);
+
+            return Ok(coins); 
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Hubo un error al obtener las monedas: {ex.Message}");
+        }
+    }
+
+}

e9b210fe2122bfe39f426def89e0a254a539ec10
Raquel <rlopber070@g.educaand.es>
2025-03-11 20:14:19 +0100

    CSS stripe

--- frontend/the-enigma-casino-client/src/features/payment/pages/PaymentPage.tsx
+++ frontend/the-enigma-casino-client/src/features/payment/pages/PaymentPage.tsx
@@ -1,30 +1,37 @@
 import { useUnit } from "effector-react";
 import EthereumPayment from "../components/EthereumPayment";
 import StripePayment from "../components/StripePayment";
-import CoinsCards from "../components/CoinsCard";
-import { $paymentMethod } from "../../catalog/store/catalogStore";
+import CoinsCards from "./../../catalog/components/CoinsCard";
+import {
+  $paymentMethod,
+  $selectedCard,
+} from "../../catalog/store/catalogStore";
 
 function PaymentPage() {
   const method = useUnit($paymentMethod);
-
+  const coinCard = useUnit($selectedCard);
   console.log("Método de pago seleccionado:", method);
 
   return (
     <>
       <div className="h-full bg-Background-Page flex justify-center">
         <div className="grid grid-cols-2 gap-30 w-full justify-center">
-          <div className="col-start-1 row-start-1 place-self-center  ml-[-5rem]">
+          <div className="col-start-1 row-start-1 place-self-center ml-[-5rem]">
             <CoinsCards
-              id={1}
-              price={1}
-              quantity={1}
-              image="url"
-              offer={0}
+              key={coinCard.id}
+              id={coinCard.id}
+              price={coinCard.price}
+              quantity={coinCard.quantity}
+              image={coinCard.image}
+              offer={coinCard.offer}
+              isSelected={false}
+              onSelect={() => {}}
               size="large"
+              clickable={false}
             />
           </div>
 
-          <div className="col-start-2 row-start-1 flex place-self-center justify-self-end">
+          <div className="col-start-2 row-start-1 flex place-self-center justify-center">
             {method === "Stripe" && <StripePayment />}
             {method === "Ethereum" && <EthereumPayment />}
           </div>

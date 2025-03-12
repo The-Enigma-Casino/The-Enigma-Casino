2a19e7bb1eb127ac5a0b2d85e590434442c67997
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-12 18:38:28 +0100

    SidebarMenu comenzado

---
+++ frontend/the-enigma-casino-client/src/components/layouts/sidebarMenu/SidebarMenu.tsx
@@ -0,0 +1,7 @@
+function SidebarMenu() {
+  return <nav >
+    <h1>SidebarMenu</h1>
+  </nav >;
+}
+
+export default SidebarMenu;
--- frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
+++ frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
@@ -1,8 +1,10 @@
+import SidebarMenu from "../../../components/layouts/sidebarMenu/SidebarMenu";
 import Carousel from "../components/ui/Carousel";
 
 function Home() {
   return (
     <>
+      <SidebarMenu />
       <Carousel />
     </>
   );

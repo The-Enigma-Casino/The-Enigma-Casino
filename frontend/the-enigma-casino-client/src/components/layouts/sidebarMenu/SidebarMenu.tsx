c8e63355d1be6f56f97cd685263856bbd056b14f
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-12 20:13:17 +0100

    SidebarMenu avanzado y cambio de color

--- frontend/the-enigma-casino-client/src/components/layouts/sidebarMenu/SidebarMenu.tsx
+++ frontend/the-enigma-casino-client/src/components/layouts/sidebarMenu/SidebarMenu.tsx
@@ -1,7 +1,51 @@
+import classes from "./SidebarMenu.module.css";
+import Button from "../../ui/button/Button";
+import { useNavigate } from "react-router-dom";
+import { $token } from "../../../features/auth/store/authStore";
+import { useEffect } from "react";
+import { useUnit } from "effector-react";
+
 function SidebarMenu() {
-  return <nav >
-    <h1>SidebarMenu</h1>
-  </nav >;
+  const userLive = 777;
+  const token = useUnit($token);
+  const navigate = useNavigate();
+
+  useEffect(() => {}, [token]);
+
+  return (
+    <nav className={classes.sidebarMenu}>
+      <div className={classes.container}>
+        <div className={classes.profile}>
+          <img src="/svg/user.svg" alt="Imagen Perfil" />
+          <h2>Inicio de sesión.</h2>
+        </div>
+        <div className={classes.menu}>
+          <Button color="yellow" variant="large" font="bold" onClick={() => navigate("/catalog")}>
+            Fichas
+          </Button>
+
+          {token ? (
+            <Button color="green" variant="large" font="bold" onClick={() => navigate("/")}>
+              Amigos
+            </Button>
+          ) : (
+            <>
+              <Button color="green" variant="large" font="bold" onClick={() => navigate("/auth/login")}>
+                Iniciar sesión
+              </Button>
+              <Button color="green" variant="large" font="bold" onClick={() => navigate("/auth/register")}>
+                Registro
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+      <div className={classes.totalUser}>
+        <img src="/svg/user-live.svg" alt="" />
+        <p>{userLive}</p>
+      </div>
+    </nav>
+  );
 }
 
 export default SidebarMenu;

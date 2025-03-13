cf7b8f914ff6c008142dcbfda60fd6d57a9e50e2
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-13 18:28:10 +0100

    SidebarMenu y Name

--- frontend/the-enigma-casino-client/src/components/layouts/sidebarMenu/SidebarMenu.tsx
+++ frontend/the-enigma-casino-client/src/components/layouts/sidebarMenu/SidebarMenu.tsx
@@ -1,39 +1,85 @@
 import classes from "./SidebarMenu.module.css";
 import Button from "../../ui/button/Button";
 import { useNavigate } from "react-router-dom";
-import { $token } from "../../../features/auth/store/authStore";
+import {
+  $name,
+  $token,
+  loadName,
+} from "../../../features/auth/store/authStore";
 import { useEffect } from "react";
 import { useUnit } from "effector-react";
+import FriendsPanel from "../../../features/friends/components/layouts/friendsPanel";
 
 function SidebarMenu() {
-  const userLive = 777;
+  const userLive: number = 777;
   const token = useUnit($token);
+  const name = useUnit($name);
   const navigate = useNavigate();
 
-  useEffect(() => {}, [token]);
+  useEffect(() => {
+    loadName();
+  }, [token]);
 
   return (
     <nav className={classes.sidebarMenu}>
       <div className={classes.container}>
         <div className={classes.profile}>
           <img src="/svg/user.svg" alt="Imagen Perfil" />
-          <h2>Inicio de sesión.</h2>
+          {token ? (
+            <>
+              <h2>{name}</h2>
+              <Button
+                color="green"
+                variant="small"
+                font="bold"
+                onClick={() => navigate("/")}
+              >
+                Perfil
+              </Button>
+            </>
+          ) : (
+            <h2>Inicie Sesión</h2>
+          )}
         </div>
         <div className={classes.menu}>
-          <Button color="yellow" variant="large" font="bold" onClick={() => navigate("/catalog")}>
+          <Button
+            color="yellow"
+            variant="large"
+            font="bold"
+            onClick={() => navigate("/catalog")}
+          >
             Fichas
           </Button>
 
           {token ? (
-            <Button color="green" variant="large" font="bold" onClick={() => navigate("/")}>
-              Amigos
-            </Button>
+            <>
+              <Button
+                color="green"
+                variant="large"
+                font="bold"
+                onClick={() => navigate("/")}
+              >
+                Amigos
+              </Button>
+              <h2 className={classes.titleFriend}>AMIGOS EN LÍNEA</h2>
+              <FriendsPanel />
+            </>
           ) : (
             <>
-              <Button color="green" variant="large" font="bold" onClick={() => navigate("/auth/login")}>
+              <Button
+                color="green"
+                variant="large"
+                font="bold"
+                onClick={() => navigate("/auth/login")}
+              >
                 Iniciar sesión
               </Button>
-              <Button color="green" variant="large" font="bold" onClick={() => navigate("/auth/register")}>
+              <Button
+                color="green"
+                variant="large"
+                font="bold"
+                onClick={() => navigate("/auth/register")}
+              >
                 Registro
               </Button>
             </>

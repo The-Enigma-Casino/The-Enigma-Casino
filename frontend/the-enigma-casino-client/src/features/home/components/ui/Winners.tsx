2b0928da6ba9aeaf9646dd7f783f3ed3acb1a03d
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-14 19:01:42 +0100

    Winners casi terminado

--- frontend/the-enigma-casino-client/src/features/home/components/ui/Winners.tsx
+++ frontend/the-enigma-casino-client/src/features/home/components/ui/Winners.tsx
@@ -1,8 +1,34 @@
+import { formatPriceWithCurrency } from "../../../../utils/priceUtils";
+
+interface Player {
+  name: string;
+  cent: number;
+}
 
 function Winners() {
+  const players: Player[] = [
+    { name: "Alejandro", cent: 77700 },
+    { name: "Jose", cent: 35000 },
+    { name: "Raquel", cent: 1000000000 },
+    { name: "Rocio", cent: 4000 },
+    { name: "Jose S", cent: 5000 },
+    { name: "Fernando", cent: 1000 },
+    { name: "Pablo", cent: 2000 },
+    { name: "Mikel", cent: 3000 },
+    { name: "Antoñito", cent: 4000 },
+    { name: "David", cent: 6000 },
+  ];
+
   return (
-    <div>
-      <h1>Winners</h1>
+    <div className="p-6 text-white text-center bg-[#2e2e2e] rounded-full">
+      <p className="text-lg">
+        {players.map((player, index) => (
+          <span key={index}>
+            {player.name} - {formatPriceWithCurrency(player.cent)}
+            {index < players.length - 1 ? " | " : ""}
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
--- frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
+++ frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
@@ -1,6 +1,7 @@
 import SidebarMenu from "../../../components/layouts/sidebarMenu/SidebarMenu";
 import GamePanel from "../components/layouts/GamePanel";
 import Carousel from "../components/ui/Carousel";
+import Winners from "../components/ui/Winners";
 import classes from "./Home.module.css"; // Importamos los estilos en CSS puro
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <SidebarMenu />
       <section className={classes.homeSection}>
         <Carousel />
+        <Winners />
         <GamePanel />
       </section>
     </div>

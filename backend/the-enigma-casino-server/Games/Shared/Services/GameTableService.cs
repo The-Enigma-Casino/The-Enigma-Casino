daa0c046c7549fce2da901a32ed9e88d7447422e
Raquel <rlopber070@g.educaand.es>
2025-03-23 18:07:32 +0100

    Repository, Service, Controller for GameTable

---
+++ backend/the-enigma-casino-server/Games/Shared/Services/GameTableService.cs
@@ -0,0 +1,20 @@
+﻿using the_enigma_casino_server.Games.Shared.Entities;
+using the_enigma_casino_server.Games.Shared.Entities.Enum;
+using the_enigma_casino_server.Models.Database;
+
+namespace the_enigma_casino_server.Games.Shared.Services;
+
+public class GametableService
+{
+    private readonly UnitOfWork _unitOfWork;
+
+    public GametableService(UnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<List<GameTable>> GetTablesByGameTypeAsync(GameType gameType)
+    {
+        return await _unitOfWork.GameTableRepository.GetByGameTypeAsync(gameType);
+    }
+}

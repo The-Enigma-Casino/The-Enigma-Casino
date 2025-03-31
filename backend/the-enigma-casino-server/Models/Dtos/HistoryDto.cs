f9ca8fa5d397979c63a1059f668783184880d4c4
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-03-31 20:28:22 +0200

    Histortial comenzado

---
+++ backend/the-enigma-casino-server/Models/Dtos/HistoryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace the_enigma_casino_server.Models.Dtos;
+
+public class HistoryDto
+{
+    public List<GamesDto> GamesDtos { get; set; }
+
+    public int TotalPages { get; set; }
+
+    public HistoryDto(List<GamesDto> gamesDtos, int pages)
+    {
+        GamesDtos = gamesDtos;
+        TotalPages = pages;
+    }
+}
---
+++ backend/the-enigma-casino-server/Models/Mappers/HistoryMapper.cs
@@ -0,0 +1,12 @@
+﻿using the_enigma_casino_server.Models.Dtos;
+
+namespace the_enigma_casino_server.Models.Mappers;
+
+public class HistoryMapper
+{
+    //public GamesDto ToHistory()
+    //{
+
+    //}
+
+}

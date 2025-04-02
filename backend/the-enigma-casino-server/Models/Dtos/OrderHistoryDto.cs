8bbb9f951cef8b42919170d050102fe903fb9500
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-04-02 20:22:18 +0200

    Dto y mapper

---
+++ backend/the-enigma-casino-server/Models/Dtos/OrderHistoryDto.cs
@@ -0,0 +1,34 @@
+﻿using the_enigma_casino_server.Models.Database.Entities.Enum;
+
+namespace the_enigma_casino_server.Models.Dtos;
+
+public class OrderHistoryDto
+{
+    public int Id { get; set; }
+
+    public DateTime PaidDate { get; set; }
+
+    public string Image { get; set; }
+
+    public int Price { get; set; }
+
+    public int Coins { get; set; }
+
+    public PayMode PayMode { get; set; }
+
+    public OrderType OrderType { get; set; }
+
+    public OrderHistoryDto() { }
+
+    public OrderHistoryDto(int id, DateTime paidDate, int price, int coins, PayMode payMode, OrderType orderType)
+    {
+        Id = id;
+        PaidDate = paidDate;
+        Image = "";
+        Price = price;
+        Coins = coins;
+        PayMode = payMode;
+        OrderType = orderType;
+    }
+
+}
--- backend/the-enigma-casino-server/Models/Mappers/OrderMapper.cs
+++ backend/the-enigma-casino-server/Models/Mappers/OrderMapper.cs
@@ -1,41 +1,66 @@
-﻿using the_enigma_casino_server.Models.Database.Entities;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Stripe;
+using the_enigma_casino_server.Models.Database.Entities;
+using the_enigma_casino_server.Models.Database.Entities.Enum;
 using the_enigma_casino_server.Models.Dtos;
 using the_enigma_casino_server.Utilities;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace the_enigma_casino_server.Models.Mappers
+namespace the_enigma_casino_server.Models.Mappers;
+
+public class OrderMapper
 {
-    public class OrderMapper
+    public OrderDto ToOrderDto(Order order)
     {
-        public OrderDto ToOrderDto(Order order)
-        {
-            OrderDto orderDto = new OrderDto(order.Id, order.CoinsPack, order.IsPaid, order.PaidDate, order.PayMode );
-            orderDto.Coins = Coinsify.CoinsifyByCent(order.Price);
+        OrderDto orderDto = new OrderDto(order.Id, order.CoinsPack, order.IsPaid, order.PaidDate, order.PayMode );
+        orderDto.Coins = Coinsify.CoinsifyByCent(order.Price);
 
-            return orderDto;
-        }
-
-        public OrderDto ToOrderDtoWithCoinsPackDto(Order order)
-        {
-            
-            CoinsPackDto coinsPackDto = new CoinsPackDto
-            {
-                Id = order.CoinsPack.Id,       
-                Price = order.CoinsPack.Price
-            };
-
-            
-            OrderDto orderDto = new OrderDto(
-                order.Id,                         
-                coinsPackDto,                     
-                order.IsPaid,                     
-                order.PaidDate,                   
-                order.PayMode                     
-            );
-
-            
-            orderDto.Coins = Coinsify.CoinsifyByCent(order.Price);
-
-            return orderDto;  
-        }
+        return orderDto;
     }
+
+    public OrderDto ToOrderDtoWithCoinsPackDto(Order order)
+    {
+        
+        CoinsPackDto coinsPackDto = new CoinsPackDto
+        {
+            Id = order.CoinsPack.Id,       
+            Price = order.CoinsPack.Price
+        };
+
+        
+        OrderDto orderDto = new OrderDto(
+            order.Id,                         
+            coinsPackDto,                     
+            order.IsPaid,                     
+            order.PaidDate,                   
+            order.PayMode                     
+        );
+
+        
+        orderDto.Coins = Coinsify.CoinsifyByCent(order.Price);
+        return orderDto;
+    }
+
+    public OrderHistoryDto ToOrderHistoryDto(Order order)
+    {
+        OrderHistoryDto orderHistoryDto = new OrderHistoryDto(order.Id, order.PaidDate, order.Price, order.Coins, order.PayMode, order.OrderType);
+
+        if (order.OrderType == OrderType.Purchase)
+            orderHistoryDto.Image = order.CoinsPack.Image;
+
+        return orderHistoryDto;
+    }
+
+    public List<OrderHistoryDto> ToListOrderHistoryDto(List<Order> orders)
+    {
+        List<OrderHistoryDto> orderHistoryDtos = new List<OrderHistoryDto>();
+
+        foreach (Order order in orders)
+        {
+            orderHistoryDtos.Add(ToOrderHistoryDto(order));
+        }
+
+        return orderHistoryDtos;
+    }
+
 }

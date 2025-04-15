9903f32166997108dde285240512eeee0e1878fd
Raquel <rlopber070@g.educaand.es>
2025-04-15 20:12:23 +0200

    Ruleta preparada para WS

---
+++ backend/the-enigma-casino-server/Games/Roulette/RouletteWheel.cs
@@ -0,0 +1,22 @@
+﻿namespace the_enigma_casino_server.Games.Roulette;
+
+public class RouletteWheel
+{
+    private static readonly Random _random = new();
+
+    private static readonly HashSet<int> RedNumbers = new()
+    {
+        1, 3, 5, 7, 9, 12, 14, 16, 18,
+        19, 21, 23, 25, 27, 30, 32, 34, 36
+    };
+
+    public int Spin()
+    {
+        return _random.Next(0, 37);
+    }
+
+    public static string GetColor(int number)
+    {
+        return number == 0 ? "green" : RedNumbers.Contains(number) ? "red" : "black";
+    }
+}

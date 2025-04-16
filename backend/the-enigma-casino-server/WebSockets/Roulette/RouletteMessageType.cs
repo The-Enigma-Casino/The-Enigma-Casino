a2f040f0ce270c8841eaf1d343534a49aad10fe3
José Molina <josemolinamelendez@gmail.com>
2025-04-16 18:25:17 +0200

    Feat/ROULETTE-HandleAsync

---
+++ backend/the-enigma-casino-server/WebSockets/Roulette/RouletteMessageType.cs
@@ -0,0 +1,9 @@
+﻿namespace the_enigma_casino_server.Websockets.Roulette;
+
+public static class RouletteMessageType
+{
+    public const string PlaceBet = "place_bet";
+    public const string Spin = "spin";
+    public const string GameState = "game_state";
+    public const string SpinResult = "spin_result";
+}
---
+++ backend/the-enigma-casino-server/WebSockets/Roulette/RouletteWS.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using the_enigma_casino_server.WebSockets.Base;
+using the_enigma_casino_server.WebSockets.Interfaces;
+
+namespace the_enigma_casino_server.Websockets.Roulette;
+
+public class RouletteWS : BaseWebSocketHandler, IWebSocketMessageHandler, IGameTurnService
+{
+    public string Type => "roulette";
+
+    public RouletteWS(ConnectionManagerWS connectionManagerWS, IServiceProvider serviceProvider) : base(connectionManagerWS, serviceProvider) { }
+
+
+    public async Task HandleAsync(string userId, JsonElement message)
+    {
+        if (message.TryGetProperty("action", out JsonElement actionProp))
+        {
+            string action = actionProp.GetString();
+            await (action switch {
+                RouletteMessageType.PlaceBet => HandlePlaceBetAsync(userId, message),
+                RouletteMessageType.Spin => HandleSpinAsync(userId, message),
+                _ => Task.CompletedTask
+            });
+
+        }
+    }
+
+    public Task ForceAdvanceTurnAsync(int tableId, int userId)
+    {
+        // En ruleta normalmente no hay turnos, pero dejamos esto por compatibilidad
+        return Task.CompletedTask;
+    }
+
+    private Task HandlePlaceBetAsync(string userId, JsonElement message)
+    {
+        // Lo haremos en el próximo paso
+        return Task.CompletedTask;
+    }
+
+    private Task HandleSpinAsync(string userId, JsonElement message)
+    {
+        // Lo haremos luego
+        return Task.CompletedTask;
+    }
+}

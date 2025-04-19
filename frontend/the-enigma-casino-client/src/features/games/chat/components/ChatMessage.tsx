7ccaa6cec69ecdb946b7725f8fe3a8cd9adfc4f2
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-04-19 11:41:47 +0200

    Foto en el chat

--- frontend/the-enigma-casino-client/src/features/games/chat/components/ChatMessage.tsx
+++ frontend/the-enigma-casino-client/src/features/games/chat/components/ChatMessage.tsx
@@ -1,3 +1,4 @@
+import { USER_IMAGES } from "../../../../config";
 import styles from "./ChatMessage.module.css";
 
 interface ChatMessageProps {
@@ -12,7 +13,7 @@ export const ChatMessage = ({ isOwn = false, username, message, avatarUrl }: Cha
     <div className={`${styles.messageWrapper} ${isOwn ? styles.own : styles.other}`}>
       <div className={styles.bubble}>
         <div className={styles.header}>
-          <img src={avatarUrl} alt="Avatar" className={styles.avatar} />
+          <img src={USER_IMAGES + "/" + avatarUrl} alt="Avatar" className={styles.avatar} />
           {!isOwn && username && <span className={styles.username}>{username}</span>}
         </div>
         <div className={styles.text}>{message}</div>

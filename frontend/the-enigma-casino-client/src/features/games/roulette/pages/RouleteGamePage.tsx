d7b0de94651ab84413e72b6a2be22f9c5c786234
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-04-20 11:58:11 +0200

    Quitar la ruleta

--- frontend/the-enigma-casino-client/src/features/games/roulette/pages/RouleteGamePage.tsx
+++ frontend/the-enigma-casino-client/src/features/games/roulette/pages/RouleteGamePage.tsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
-import RouletteWheel from "../components/RouletteWheel";
+import { RouletteBetBoard } from "../components/RouletteBetBoard";
+// import RouletteWheel from "../components/RouletteWheel";
 
 function RouletteGamePage() {
   const [number, setNumber] = useState(0);
+  const [color, setColor] = useState("black");
 
   const handleSpin = () => {
     const result = Math.floor(Math.random() * 37);
     setNumber(result);
+    setColor("red");
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
-      <h1 className="text-4xl font-bold mb-6">Ruleta</h1>
-      <RouletteWheel winningNumber={number} />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-900 bg-repeat p-6 text-white font-mono">
+      <h1 className="text-7xl text-center font-bold mb-6 drop-shadow">♠️ Ruleta</h1>
+      <h2 className="text-2xl mb-4">Número ganador: {number}</h2>
+      <h2 className="text-2xl mb-4">Color ganandor: {color}</h2>
+      {/* <RouletteWheel winningNumber={number} /> */}
       <button
         onClick={handleSpin}
         className="mt-6 px-6 py-3 bg-yellow-400 hover:bg-yellow-500 text-black font-bold rounded"
       >
         Girar
       </button>
+      <RouletteBetBoard />
     </div>
   );
 }

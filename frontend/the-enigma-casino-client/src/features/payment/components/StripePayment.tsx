5887ae619e8f1888843a6aff64d578403310d615
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2025-04-23 20:26:24 +0200

    Nombres de css

--- frontend/the-enigma-casino-client/src/features/payment/components/StripePayment.tsx
+++ frontend/the-enigma-casino-client/src/features/payment/components/StripePayment.tsx
@@ -20,7 +20,7 @@ import {
   $paymentStatus,
 } from "../store/PaymentStore";
 
-import styles from "./StripePayment.module.css";
+import styles from "./stripePayment.module.css";
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 

8ec85aaa3e08d315fe58fe85e772d6eff12bd048
Jmolmel <josemolinamelendez@gmail.com>
2025-05-12 19:29:13 +0200

    Feat/BACK-Config
    Novato

---
+++ backend/the-enigma-casino-server/Infrastructure/Database/Config/FriendRequestConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using the_enigma_casino_server.Core.Entities;
+
+namespace the_enigma_casino_server.Infrastructure.Database.Config;
+
+public class FriendRequestConfig : IEntityTypeConfiguration<FriendRequest>
+{
+    public void Configure(EntityTypeBuilder<FriendRequest> builder)
+    {
+        builder.ToTable("friendRequests");
+
+        builder.HasKey(fr => fr.Id);
+
+        builder.Property(fr => fr.SenderId)
+               .IsRequired();
+
+        builder.Property(fr => fr.ReceiverId)
+               .IsRequired();
+
+        builder.Property(fr => fr.SentAt)
+               .IsRequired();
+
+        builder.Property(fr => fr.Status)
+               .IsRequired();
+    }
+}

46a4621366704db5aaf197cdc67b62309c17b80d
José Molina <josemolinamelendez@gmail.com>
2025-05-19 00:55:29 +0200

    Feat/FriendPanelOnline

--- frontend/the-enigma-casino-client/src/features/friends/components/layouts/FriendsPanel.tsx
+++ frontend/the-enigma-casino-client/src/features/friends/components/layouts/FriendsPanel.tsx
@@ -1,35 +1,40 @@
 import { useUnit } from "effector-react";
-import { $userId } from "../../../auth/store/authStore";
 import classes from "./FriendsPanel.module.css";
 import { Link } from "react-router-dom";
 import { encodeId } from "../../../../utils/sqidUtils";
+import { $friends } from "../../stores/friends.store";
+import { useEffect } from "react";
+import { fetchFriendsFx, getOnlineFriendsRequested } from "../../stores";
+import { IMAGE_PROFILE_URL } from "../../../../config";
 
 function FriendsPanel() {
-  const friends = [
-    { id: 1, name: "Jose", imgSrc: "https://avatars.githubusercontent.com/u/146203038?v=4" },
-    { id: 2, name: "Raquel", imgSrc: "https://avatars.githubusercontent.com/u/148492169?v=4" },
-    { id: 3, name: "Alejandro", imgSrc: "https://avatars.githubusercontent.com/u/146462406?v=4" },
-    { id: 4, name: "Vegetta", imgSrc: "https://i1.sndcdn.com/artworks-H9rh5FQGHtqUX1yy-XT0gtg-t500x500.jpg" }
-  ];
-  const userId = useUnit($userId);
+  const friends = useUnit($friends);
 
+  useEffect(() => {
+    fetchFriendsFx().then(() => {
+      getOnlineFriendsRequested();
+    });
+  }, []);
+
+  const onlineFriends = friends.filter(f => f.isOnline);
   return (
     <div className={classes.friendsPanel}>
       <div className={classes.friendsPanelContainer}>
         <div className={classes.friends}>
-          {friends.map(friend => {
+          {onlineFriends.map(friend => {
             const encoded = encodeId(friend.id);
-
             return (
               <div key={friend.id} className={classes.friend}>
                 <Link to={`/profile/${encoded}`}>
-                  <img src={friend.imgSrc} alt={`Imagen de ${friend.name}`} />
+                  <img src={`${IMAGE_PROFILE_URL}${friend.image}`} alt={`Imagen de ${friend.nickName}`} />
                 </Link>
-                <h3>{friend.name}</h3>
+                <h3>{friend.nickName}</h3>
               </div>
             );
           })}
-
+          {onlineFriends.length === 0 && (
+            <p className="text-gray-400 text-xl">No hay amigos conectados.</p>
+          )}
         </div>
       </div>
     </div >

d1aa1ff88b2653b0dff7de6c97b2e365cd124de5
Rlopber <ralober92@gmail.com>
2025-05-20 12:24:50 +0200

    Home responsive

--- frontend/the-enigma-casino-client/src/features/home/components/ui/CardGame.tsx
+++ frontend/the-enigma-casino-client/src/features/home/components/ui/CardGame.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import classes from "./CardGame.module.css";
 
 interface CardGameProps {
   imageUrl: string;
@@ -12,17 +11,22 @@ const CardGame: React.FC<CardGameProps> = ({ imageUrl, altText, gameType, name }
   const navigate = useNavigate();
 
   return (
-    <div className={classes.cardGameContainer} onClick={() => navigate(`/tables/${gameType}`)}>
-      <div className={classes.cardGameImage}>
+    <div
+      onClick={() => navigate(`/tables/${gameType}`)}
+      className="flex flex-col items-center justify-center w-[18rem] h-[20rem] bg-Background-Overlay rounded-[40px] text-white gap-6 cursor-pointer transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:border-2 hover:border-Principal"
+    >
+      <div className="w-[14rem] h-[14rem] rounded-full overflow-hidden flex justify-center items-center">
         <img
           src={imageUrl}
           alt={altText}
+          className="w-full h-full object-cover"
         />
       </div>
-      <h2 className={classes.title}>{name}</h2>
+      <h2 className="text-3xl text-white text-center">{name}</h2>
     </div>
   );
 };
+
 
 
 export default CardGame;
--- frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
+++ frontend/the-enigma-casino-client/src/features/home/pages/Home.tsx
@@ -2,13 +2,15 @@ import SidebarMenu from "../../../components/layouts/sidebarMenu/SidebarMenu";
 import GamePanel from "../components/layouts/GamePanel";
 import Carousel from "../components/ui/Carousel";
 import Winners from "../components/ui/Winners";
-import classes from "./Home.module.css"; // Importamos los estilos en CSS puro
 
 function Home() {
   return (
-    <div className={classes.homeContainer}>
-      <SidebarMenu />
-      <section className={classes.homeSection}>
+    <div className="flex h-screen w-full max-w-full overflow-x-clip bg-Background-Page">
+      <div className="hidden md:block w-[280px] shrink-0">
+        <SidebarMenu />
+      </div>
+
+      <section className="flex flex-col items-center gap-8 w-full">
         <Carousel />
         <Winners />
         <GamePanel />

80f34789963bd74c1f6e7ff1dda344adb973fec4
Raquel <rlopber070@g.educaand.es>
2025-05-23 19:09:03 +0200

    Reconexion a websocket

--- frontend/the-enigma-casino-client/src/App.tsx
+++ frontend/the-enigma-casino-client/src/App.tsx
@@ -4,13 +4,18 @@ import "./App.css";
 import router from "./routes";
 import { RouterProvider } from "react-router-dom";
 
-
-import "./features/gameTables/store/tableHandler"; // NO BORRAR
+import "./features/gameTables/store/tableHandler";
 import "./features/friends/stores/friends.handler";
 import "./features/friends/stores/friends.watchers";
 import "./features/friends/stores/friends.samples";
+import { useEffect } from "react";
+import { connectSocket } from "./websocket/store/wsIndex";
 
 function App() {
+  useEffect(() => {
+    connectSocket();
+  }, []);
+
   return (
     <>
       <Toaster

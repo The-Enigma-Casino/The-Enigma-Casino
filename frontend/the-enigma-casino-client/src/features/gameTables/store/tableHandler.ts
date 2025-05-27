071c6a7d23261194726bd61080f53297072926e3
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-05-27 17:02:29 +0200

    Mesas sin terminar
    - Controla algo mejor los toast
    TODO:
    - Falta por terminar el join_denied
    - Fatla implementar algunos mensajes
    - Falta mejorarlo en general

--- frontend/the-enigma-casino-client/src/features/gameTables/store/tableHandler.ts
+++ frontend/the-enigma-casino-client/src/features/gameTables/store/tableHandler.ts
@@ -1,14 +1,20 @@
 import toast from "react-hot-toast";
 import { socketMessageReceived } from "../../../websocket/store/wsEvents";
 import { Player } from "../models/GameTable.interface";
-import { countdownStarted, countdownStopped, gameStarted, setPendingJoinTableId, tableUpdated } from "./tablesEvents"; // errorReceived añadir para toast
+import {
+  countdownStarted,
+  countdownStopped,
+  gameStarted,
+  setPendingJoinTableId,
+  tableUpdated,
+} from "./tablesEvents"; // errorReceived añadir para toast
 import { navigateTo } from "../../games/shared/router/navigateFx";
 import { stopGameLoading } from "../../friends/stores/friends.events";
 
-
 // Mensajes de error traducidos
 const errorMessageMap: Record<string, string> = {
-  already_left: "Has abandonado recientemente esta mesa. Espera a que termine la partida en curso o únete a otra mesa.",
+  already_left:
+    "Has abandonado recientemente esta mesa. Espera a que termine la partida en curso o únete a otra mesa.",
   not_enough_coins: "No tienes suficientes fichas para unirte.",
   table_full: "La mesa está llena.",
   maintenance: "Esta mesa está en mantenimiento.",
@@ -58,18 +64,33 @@ socketMessageReceived.watch((data) => {
       navigateTo(gameViewPath);
       break;
     }
-
-    case "error": {
-      const rawMessage = data.message;
-      const userMessage =
-        errorMessageMap[rawMessage] || "No se pudo procesar tu solicitud.";
-
-      if (typeof rawMessage === "string") {
-        toast.error(userMessage);
-        // errorReceived(rawMessage);  Controlar el error que siempre llega
-      }
+    case "waiting_opponent": {
+      toast(
+        "👤 " +
+          (data.message ??
+            "Esperando más jugadores para comenzar la partida...")
+      );
       break;
     }
+    case "error": {
+      const errorKey = data.reason || data.message;
+      const userMessage =
+        errorMessageMap[errorKey] ||
+        data.message ||
+        "No se pudo procesar tu solicitud.";
+
+      toast.error(userMessage);
+      break;
+    }
+    case "join_denied":
+      toast.error(data.reason);
+      break;
+
+    case "table_closed": {
+      toast.error(data.reason);
+      break;
+    }
+    default:
+      break;
   }
 });
-

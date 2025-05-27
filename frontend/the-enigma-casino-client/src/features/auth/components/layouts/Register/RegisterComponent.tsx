9b568e8253751f34640845096f674091752dc4a4
Rlopber <ralober92@gmail.com>
2025-05-27 17:01:47 +0200

    comienzo de registro responsive

--- frontend/the-enigma-casino-client/src/features/auth/components/layouts/Register/RegisterComponent.tsx
+++ frontend/the-enigma-casino-client/src/features/auth/components/layouts/Register/RegisterComponent.tsx
@@ -1,30 +1,46 @@
-import classes from "./Register.module.css";
 import { useNavigate } from "react-router-dom";
 import RegisterFormSection from "./RegisterFormSection";
+import classes from "./Register.module.css"; // contiene solo .registerLeft
 
 function RegisterComponent() {
   const navigate = useNavigate();
 
   return (
-    <div className={classes.register}>
-      <div className={classes.registerLeft}>
-        <h1 className={classes.title}>REGISTRO</h1>
-
-        <div className={classes.registerLogo}>
-          <img
-            src="/img/icono.webp"
-            alt="Logo Enigma"
-            onClick={() => navigate("/")}
-          />
+    <div className="flex flex-col lg:flex-row w-full h-full lg:h-screen bg-background-page">
+      <div
+        className={`${classes.registerLeft} hidden lg:flex flex-col items-start justify-center pl-[8%] gap-12`}
+      >
+        <div className="text-center">
+          <p className="text-white text-6xl font-bold">¿Ya tienes cuenta?</p>
+          <button
+            onClick={() => navigate("/auth/login")}
+            className="text-black text-3xl underline hover:text-principal transition-colors mt-2 font-semibold"
+          >
+            Inicia sesión aquí
+          </button>
         </div>
 
-        <a onClick={() => navigate("/auth/login")}>
-          <p>¿Tienes cuenta?</p>
-        </a>
+        <img
+          src="/img/icono.webp"
+          alt="Logo Enigma"
+          className="w-[200px] sm:w-[240px] md:w-[280px] lg:w-[260px] xl:w-[320px] cursor-pointer"
+          onClick={() => navigate("/")}
+        />
       </div>
 
-      <div className={classes.registerRight}>
+      <div className="flex flex-col items-center justify-center w-full flex-grow lg:w-[60%] lg:mr-40 lg:mb-32">
         <RegisterFormSection />
+      </div>
+
+      {/* FOOTER EN MÓVIL */}
+      <div className="flex-shrink-0 flex flex-col items-center justify-center bg-[var(--Principal)] text-center text-white py-6 px-6 w-full lg:hidden">
+        <p className="text-4xl text-black font-bold">¿Ya tienes cuenta?</p>
+        <button
+          onClick={() => navigate("/auth/login")}
+          className="text-2xl underline hover:text-principal transition-colors mt-2 font-semibold"
+        >
+          Inicia sesión aquí
+        </button>
       </div>
     </div>
   );
--- frontend/the-enigma-casino-client/src/features/auth/components/layouts/Register/RegisterFormSection.tsx
+++ frontend/the-enigma-casino-client/src/features/auth/components/layouts/Register/RegisterFormSection.tsx
@@ -1,6 +1,6 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import classes from "./Register.module.css";
+import classes from "./RegisterForm.module.css";
 import { useRegisterForm } from "./UseRegisterForm";
 import Input from "../../../../../components/ui/input/CustomInput";
 import InputDebounce from "../../ui/InputDebounce";
@@ -24,101 +24,107 @@ function RegisterFormSection() {
   registerLocale("es", es);
 
   return (
-    <form onSubmit={handleSubmit} className={classes.registerForm}>
-      <Input
-        type="text"
-        name="nickName"
-        id="nickName"
-        placeholder="Nombre de usuario"
-        value={formData.nickName}
-        onChange={handleChange}
-      />
+    <>
+      <h1 className="text-[6rem] text-Principal text-center mb-12">REGISTRO</h1>
 
-      <Input
-        type="text"
-        name="fullname"
-        id="fullname"
-        placeholder="Nombre completo"
-        value={formData.fullname}
-        onChange={handleChange}
-      />
-
-      <Input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Correo electrónico"
-        value={formData.email}
-        onChange={handleChange}
-      />
-
-      <div className={classes.datePickerContainer}>
-        <DatePicker
-          selected={
-            formData.dateOfBirth ? new Date(formData.dateOfBirth) : null
-          }
-          onChange={handleDateChange}
-          maxDate={new Date()}
-          minDate={new Date("1920-01-01")}
-          dateFormat="yyyy-MM-dd"
-          placeholderText="Selecciona tu fecha"
-          className={classes.datePickerInput}
-          locale="es"
-          renderCustomHeader={(props) => <CustomDatePickerHeader {...props} />}
-        />
-      </div>
-
-      <div className={classes.passwordContainer}>
+      <form onSubmit={handleSubmit} className={classes.registerForm}>
         <Input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Contraseña"
-          value={formData.password}
+          type="text"
+          name="nickName"
+          id="nickName"
+          placeholder="Nombre de usuario"
+          value={formData.nickName}
           onChange={handleChange}
-          showToggle
         />
 
         <Input
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          placeholder="Confirma la contraseña"
-          value={formData.confirmPassword}
+          type="text"
+          name="fullname"
+          id="fullname"
+          placeholder="Nombre completo"
+          value={formData.fullname}
           onChange={handleChange}
-          showToggle
         />
-      </div>
 
-      <Input
-        type="text"
-        name="address"
-        id="address"
-        placeholder="Dirección"
-        value={formData.address}
-        onChange={handleChange}
-      />
-
-      <InputDebounce
-        placeholder="Nacionalidad"
-        onSelect={handleCountrySelect}
-      />
-
-      <div className={classes.checkboxContainer}>
-        <Checkbox
-          labelText="Acepto la política de privacidad"
-          checked={acceptPrivacy}
-          linkHref="/policies"
-          onChange={() => setAcceptPrivacy(!acceptPrivacy)}
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Correo electrónico"
+          value={formData.email}
+          onChange={handleChange}
         />
-      </div>
 
-      <div className={classes.buttonRegister}>
-        <Button type="submit" variant="outline" color="green" font="large">
-          Registrarse
-        </Button>
-      </div>
-    </form>
+        <div className={classes.datePickerContainer}>
+          <DatePicker
+            selected={
+              formData.dateOfBirth ? new Date(formData.dateOfBirth) : null
+            }
+            onChange={handleDateChange}
+            maxDate={new Date()}
+            minDate={new Date("1920-01-01")}
+            dateFormat="dd-MM-yyyy"
+            placeholderText="Selecciona tu fecha"
+            className={classes.datePickerInput}
+            locale="es"
+            renderCustomHeader={(props) => (
+              <CustomDatePickerHeader {...props} />
+            )}
+          />
+        </div>
+
+        <div className={classes.passwordContainer}>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Contraseña"
+            value={formData.password}
+            onChange={handleChange}
+            showToggle
+          />
+
+          <Input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="Confirma la contraseña"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            showToggle
+          />
+        </div>
+
+        <Input
+          type="text"
+          name="address"
+          id="address"
+          placeholder="Dirección"
+          value={formData.address}
+          onChange={handleChange}
+        />
+
+        <InputDebounce
+          placeholder="Nacionalidad"
+          onSelect={handleCountrySelect}
+        />
+
+        <div className={classes.checkboxContainer}>
+          <Checkbox
+            labelText="Acepto la política de privacidad"
+            checked={acceptPrivacy}
+            linkHref="/policies"
+            onChange={() => setAcceptPrivacy(!acceptPrivacy)}
+          />
+        </div>
+
+        <div className={classes.buttonRegister}>
+          <Button type="submit" variant="outline" color="green" font="large">
+            Registrarse
+          </Button>
+        </div>
+      </form>
+    </>
   );
 }
 

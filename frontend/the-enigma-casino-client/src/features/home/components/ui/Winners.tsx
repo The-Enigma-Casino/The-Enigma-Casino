3a4df0bee8aa0f822e9ae8d2750b5c0ae64a50ec
José Molina <josemolinamelendez@gmail.com>
2025-06-03 20:53:12 +0200

    Feat/Winners

--- frontend/the-enigma-casino-client/src/features/home/components/ui/Winners.tsx
+++ frontend/the-enigma-casino-client/src/features/home/components/ui/Winners.tsx
@@ -10,17 +10,19 @@ function Winners() {
   }, [load]);
 
   return (
-    <div className="w-[90%] max-w-[1000px] mx-auto bg-Background-nav rounded-md overflow-hidden py-2 px-4 font-reddit text-white">
-      <div className="relative w-full h-[2.4rem] sm:h-[2.8rem]">
-        <div className="absolute top-0 left-0 h-full flex items-center animate-marquee whitespace-nowrap gap-6">
+    <div className="w-full max-w-screen-xl mx-auto bg-Background-nav rounded-xl overflow-hidden py-2 px-4 font-reddit text-white">
+
+      <div className="relative w-full h-[2.4rem] sm:h-[2.8rem] group">
+        <div className="absolute top-0 left-0 h-full flex items-center animate-marquee whitespace-nowrap gap-10 group-hover:[animation-play-state:paused] group-hover:pointer-events-none">
           {[...winners, ...winners].map((player, index) => (
             <span
               key={index}
-              className="inline-flex items-center gap-2 text-sm sm:text-base"
+              className="inline-flex items-center gap-2 text-lg sm:text-2xl"
             >
               <span className="text-white/80">{player.nickname}</span>
               <span className="text-Coins font-semibold flex items-center gap-1">
-                🪙 {player.amountWon}
+                <img src="/svg/coins.svg" alt="Coin" className="w-5 h-5" />
+                {player.amountWon}
               </span>
             </span>
           ))}

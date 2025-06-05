5600cf259e461d3f55ccfc22020b221949d351ef
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-06-05 09:22:18 +0200

    Intento numero 643

--- deploy/frontend/scripts/install.sh
+++ deploy/frontend/scripts/install.sh
@@ -4,7 +4,7 @@ echo "📦 Instalando frontend en /var/www/theenigmacasino..."
 
 # Respeta configuraciones, solo actualiza el contenido
 sudo mkdir -p /var/www/theenigmacasino
-sudo cp -r build/* /var/www/theenigmacasino/
+sudo cp -r dist/* /var/www/theenigmacasino/
 sudo chown -R www-data:www-data /var/www/theenigmacasino
 
 echo "✅ Frontend copiado correctamente."

71c15c620463b40fc3265d3b9ff7f7ac511aa177
Jmolmel <josemolinamelendez@gmail.com>
2025-06-12 23:19:43 +0200

    Refactor/Prueba-aws

--- backend/the-enigma-casino-server/Program.cs
+++ backend/the-enigma-casino-server/Program.cs
@@ -50,7 +50,7 @@ public class Program
 
         ConfigureStripe(app.Services);
 
-        app.MapGet("/api/", () => "The Enigma Casino! PRUEBA AWS");
+        app.MapGet("/api/", () => "The Enigma Casino!");
 
         Console.WriteLine("The Enigma Casino Server");
 
